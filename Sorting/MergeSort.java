a8568769c0a8db90c9fae4f511a47db68e926d02
sriram <sri412001@gmail.com>
2025-07-28 22:59:22 +0530

    merrgeSort

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,62 @@
+package Data_Structures_Algorithms.Sorting;
+import java.util.Arrays;
+/**
+ * Merge Sort is a divide-and-conquer algorithm that sorts an array by recursively
+ * dividing it into halves, sorting each half, and then merging the sorted halves.
+ * It has a time complexity of O(n log n) and is stable.
+ */
+
+class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original Array:");
+        System.out.println(Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length);
+        
+        System.out.println("Sorted Array:");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSort(int[] arr, int left, int right) {
+
+        if (right-left==1) return;
+
+        int mid = left + (right - left) / 2; // Find the middle point
+        mergeSort(arr, left, mid); // Sort first half
+        mergeSort(arr, mid, right); // Sort second half
+        merge(arr, left, mid, right); // Merge the sorted halves
+        
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        // Find sizes of two subarrays to be merged
+        int[] mix = new int[right - left];
+        int i = left, j = mid;
+        int k = 0;
+
+        while (i < mid && j < right) {
+            if (arr[i] <= arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < mid) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j < right) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(int l = 0; l < mix.length; l++) {
+            arr[left + l] = mix[l]; // Copy the sorted elements back to the original array
+        }
+    }
+}
